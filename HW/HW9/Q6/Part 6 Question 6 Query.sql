22c516342d1d2206691760b329e3cb5d5c5ac03e
MahsanGilani <mahsan_gillani@yahoo.com>
2023-09-28 19:48:11 +0330

    part 6 question 6 has been added to HW9

---
+++ HW/HW9/Q6/Part 6 Question 6 Query.sql
@@ -0,0 +1,6 @@
+SELECT customer.first_name , country.country
+FROM customer
+JOIN address ON customer.address_id = address.address_id
+JOIN city ON address.city_id = city.city_id
+JOIN country ON city.country_id = country.country_id
+WHERE country.country = 'United States' AND customer.first_name LIKE 'A%'
